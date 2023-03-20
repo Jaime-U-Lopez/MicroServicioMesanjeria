fc394f405511801bb32e8fbae3109fa775d5a952
Jaime Urrego <or4846@gmail.com>
2023-03-20 18:43:22 -0500

    my first commit

---
+++ src/test/java/com/example/servicioMesajeria/ServicioMesajeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servicioMesajeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioMesajeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
